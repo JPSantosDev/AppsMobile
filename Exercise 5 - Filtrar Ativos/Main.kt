ee2caf9936013a62b077e174625f105236be74ca
Alejandro <jp0056392@gmail.com>
2026-05-15 18:14:21 -0300

    Adição do restante dos exercícios
    exercícios 4,5,6

--- Exercise 5 - Filtrar Ativos/Main.kt
+++ Exercise 5 - Filtrar Ativos/Main.kt
@@ -1,8 +1,10 @@
 data class Curso(
     val id: Int,
-    val curso:String,
+    val nome: String,
+    val categoria: String,
+    val cargaHoraria: Int,
+    val nota: Double,
     val ativo: Boolean
-
 )
 
 fun encontrarCurso(curso: List<Curso>, id:Int): Curso? {
@@ -11,4 +13,32 @@ fun encontrarCurso(curso: List<Curso>, id:Int): Curso? {
 
 fun filtrarCurso(curso: List<Curso>): List<Curso> {
     return curso.filter{it.ativo}
+}
+
+fun rankingCurso(curso: List<Curso>): List<Curso> {
+    return curso.sortedBy { it.nota }
+}
+
+fun somaCargaCurso(curso: List<Curso>): Int {
+    val ativos = curso.filter{it.ativo}
+    var soma = 0
+
+    for(cursos in ativos){
+        soma+=cursos.cargaHoraria
+    }
+    return soma
+}
+
+fun retirarDuplicadosCurso(curso: List<Curso>): Set<Curso> {
+    val semDuplicados = curso.toMutableSet()
+    return semDuplicados
+}
+
+fun categorizarCurso(curso: List<Curso>): Map<String,List<Curso>> {
+    return curso.groupBy { it.categoria }
+}
+fun listarCategoriasUnicas(cursos: List<Curso>): List<String> {
+    return cursos.map { it.categoria }
+        .toSet()
+        .toList()
 }
